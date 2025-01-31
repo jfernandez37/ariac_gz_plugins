bb05d7cc47362193812cbcf532867fa90964b30c
Joseph Fernandez <joseph.fernandez@nist.gov>
2025-01-31 21:09:52 +0000

    Updated for new demo parts

--- src/ariac_logical_camera_plugin.cpp
+++ src/ariac_logical_camera_plugin.cpp
@@ -14,6 +14,10 @@
 #include <ariac_msgs/msg/basic_logical_camera_image.hpp>
 #include <ariac_msgs/msg/advanced_logical_camera_image.hpp>
 
+#include <aprs_interfaces/msg/tray.hpp>
+#include <aprs_interfaces/msg/slot_info.hpp>
+#include <aprs_interfaces/msg/sim_advanced_logical_camera_image.hpp>
+
 #include "ariac_gz_plugins/ariac_logical_camera_plugin.hpp"
 
 namespace ariac_sensors{
@@ -34,16 +38,18 @@ namespace ariac_sensors{
       bool publish_sensor_data_;
 
       /// List of models that the logical camera will publish
-      std::vector<std::string> parts_to_publish_;
-      std::vector<std::string> colors_;
-      std::map<std::string, int> part_types_;
-      std::map<std::string, int> part_colors_;
+      std::vector<std::string> gears_to_publish;
+      std::vector<std::string> trays_to_publish;
+      // std::vector<std::string> colors_;
+      std::map<std::string, int> gear_types;
+      std::map<std::string, int> tray_types;
+      // std::map<std::string, int> part_colors_;
 
       /// Ariac messages & ROS Publishsers for Basic & Advanced logical camera images
       rclcpp::Publisher<ariac_msgs::msg::BasicLogicalCameraImage>::SharedPtr basic_image_pub_;
       std::shared_ptr<ariac_msgs::msg::BasicLogicalCameraImage> basic_image_msg_;
-      rclcpp::Publisher<ariac_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr advanced_image_pub_;
-      std::shared_ptr<ariac_msgs::msg::AdvancedLogicalCameraImage> advanced_image_msg_;
+      rclcpp::Publisher<aprs_interfaces::msg::SimAdvancedLogicalCameraImage>::SharedPtr advanced_image_pub_;
+      std::shared_ptr<aprs_interfaces::msg::SimAdvancedLogicalCameraImage> advanced_image_msg_;
 
       /// Sensor Health Subscription
       rclcpp::Subscription<ariac_msgs::msg::Sensors>::SharedPtr sensor_health_sub_;
@@ -95,28 +101,21 @@ namespace ariac_sensors{
       _sdf->Get<std::string>("rostopic"), rclcpp::SensorDataQoS());
       impl_->basic_image_msg_ = std::make_shared<ariac_msgs::msg::BasicLogicalCameraImage>();
     } else if (impl_->camera_type_ == "advanced") {
-      impl_->advanced_image_pub_ = impl_->ros_node_->create_publisher<ariac_msgs::msg::AdvancedLogicalCameraImage>(
+      impl_->advanced_image_pub_ = impl_->ros_node_->create_publisher<aprs_interfaces::msg::SimAdvancedLogicalCameraImage>(
       _sdf->Get<std::string>("rostopic"), rclcpp::SensorDataQoS());
-      impl_->advanced_image_msg_ = std::make_shared<ariac_msgs::msg::AdvancedLogicalCameraImage>();
+      impl_->advanced_image_msg_ = std::make_shared<aprs_interfaces::msg::SimAdvancedLogicalCameraImage>();
     }
 
     // Set list of models to publish
-    impl_->parts_to_publish_ = {"pump", "battery", "regulator", "sensor"};
-    impl_->colors_ = {"red", "green", "blue", "orange", "purple"};
+    impl_->gears_to_publish = 
+    impl_->trays_to_publish = {"small_gear_tray", "medium_gear_tray", "large_gear_tray", "m2l1_kit_tray", "s2l2_kit_tray"};
 
-    impl_->part_types_ = {
-      {"battery", ariac_msgs::msg::Part::BATTERY},
-      {"pump", ariac_msgs::msg::Part::PUMP},
-      {"regulator", ariac_msgs::msg::Part::REGULATOR},
-      {"sensor", ariac_msgs::msg::Part::SENSOR},
-    };
-
-    impl_->part_colors_ = {
-      {"red", ariac_msgs::msg::Part::RED},
-      {"green", ariac_msgs::msg::Part::GREEN},
-      {"blue", ariac_msgs::msg::Part::BLUE},
-      {"purple", ariac_msgs::msg::Part::PURPLE},
-      {"orange", ariac_msgs::msg::Part::ORANGE},
+    impl_->tray_types = {
+      {"small_gear_tray", aprs_interfaces::msg::Tray::SMALL_GEAR_TRAY},
+      {"medium_gear_tray", aprs_interfaces::msg::Tray::MEDIUM_GEAR_TRAY},
+      {"large_gear_tray", aprs_interfaces::msg::Tray::LARGE_GEAR_TRAY},
+      {"m2l1_kit_tray", aprs_interfaces::msg::Tray::M2L1_KIT_TRAY},
+      {"s2l2_kit_tray", aprs_interfaces::msg::Tray::S2L2_KIT_TRAY}
     };
 
     // Subscribe to sensor health topic
@@ -133,6 +132,7 @@ namespace ariac_sensors{
   }
 
   void AriacLogicalCameraPluginPrivate::OnNewLogicalFrame(const gz::msgs::LogicalCameraImage &_gz_msg) {    
+    RCLCPP_INFO(ros_node_->get_logger(), "inside onnewlogicalframe");
     if (!publish_sensor_data_) {
       return;
     }
@@ -140,68 +140,146 @@ namespace ariac_sensors{
     geometry_msgs::msg::Pose sensor_pose;
     ros_gz_bridge::convert_gz_to_ros(_gz_msg.pose(), sensor_pose);
 
-    std::vector<ariac_msgs::msg::PartPose> parts;
-    std::vector<ariac_msgs::msg::KitTrayPose> trays;
+    // std::vector<ariac_msgs::msg::PartPose> parts;
+    std::vector<aprs_interfaces::msg::SlotInfo> gears;
+    std::vector<aprs_interfaces::msg::Tray> gear_trays;
+    std::vector<aprs_interfaces::msg::Tray> kit_trays;
 
     for (int i = 0; i < _gz_msg.model_size(); i++) {
       const auto & lc_model = _gz_msg.model(i);
-      std::string name = lc_model.name();
+      std::string name = lc_model.name();   
+      RCLCPP_INFO_STREAM(ros_node_->get_logger(), "lc_model name" << name);
 
-      if (name.find("kit_tray") != std::string::npos) {
-          ariac_msgs::msg::KitTrayPose kit_tray;
+      if (name.find("s2l2_kit_tray") == 0 || name.find("m2l1_kit_tray") == 0) {
+          aprs_interfaces::msg::Tray kit_tray;
 
-          std::string id_string = name.substr(9, 2);
-          kit_tray.id = std::stoi(id_string);
-          ros_gz_bridge::convert_gz_to_ros(lc_model.pose(), kit_tray.pose);
+          RCLCPP_INFO_STREAM(ros_node_->get_logger(), "Pub kit tray: " << name);
 
-          trays.push_back(kit_tray);
-          continue;
+          kit_tray.name = name;
+          if (name.find("s2l2") != std::string::npos){
+            kit_tray.identifier = aprs_interfaces::msg::Tray::S2L2_KIT_TRAY;
+          } else {
+            kit_tray.identifier = aprs_interfaces::msg::Tray::M2L1_KIT_TRAY;
+          }
+
+          // kit_tray.id = std::stoi(id_string);
+          ros_gz_bridge::convert_gz_to_ros(lc_model.pose(), kit_tray.tray_pose.pose);
+
+          kit_trays.push_back(kit_tray);
+          // continue;
+      }
+      
+      else if (name.find("small_gear_tray") == 0 || name.find("medium_gear_tray") == 0 || name.find("large_gear_tray") == 0) {
+        aprs_interfaces::msg::Tray gear_tray;
+
+        RCLCPP_INFO_STREAM(ros_node_->get_logger(), "Pub gear tray: " << name);
+
+        gear_tray.name = name;
+
+        if (name.find("small") != std::string::npos){
+          gear_tray.identifier = aprs_interfaces::msg::Tray::SMALL_GEAR_TRAY;
+        }
+        else if (name.find("medium") != std::string::npos){
+          gear_tray.identifier = aprs_interfaces::msg::Tray::MEDIUM_GEAR_TRAY;
+        }
+        else{
+          gear_tray.identifier = aprs_interfaces::msg::Tray::LARGE_GEAR_TRAY;
+        }
+
+        ros_gz_bridge::convert_gz_to_ros(lc_model.pose(), gear_tray.tray_pose.pose);
+
+        gear_trays.push_back(gear_tray);
       }
 
-      for(std::string part_type : parts_to_publish_){
-        if (name.find(part_type) != std::string::npos) {
-          ariac_msgs::msg::PartPose part;
+      else if (name.find("gear") != std::string::npos){
+        aprs_interfaces::msg::SlotInfo gear;
 
-          part.part.type = part_types_[part_type];
+        RCLCPP_INFO_STREAM(ros_node_->get_logger(), "Pub gear: " << name);
+
+        gear.name = name;
+
+        gear.occupied = true;
+
+        if (name.find("small") != std::string::npos){
+          gear.size = aprs_interfaces::msg::SlotInfo::SMALL;
+        }
+        else if (name.find("medium") != std::string::npos){
+          gear.size = aprs_interfaces::msg::SlotInfo::MEDIUM;
+        }
+        else{
+          gear.size = aprs_interfaces::msg::SlotInfo::LARGE;
+        }
+
+        ros_gz_bridge::convert_gz_to_ros(lc_model.pose(), gear.slot_pose.pose);
+
+        gears.push_back(gear);
+      }
+      // for(std::string part_type : parts_to_publish_){
+      //   if (name.find(part_type) != std::string::npos) {
+      //     ariac_msgs::msg::PartPose part;
+
+      //     part.part.type = part_types_[part_type];
           
-          for(std::string color : colors_){
-            if (name.find(color) != std::string::npos) {
-              part.part.color = part_colors_[color];
-            }
-          }
-          ros_gz_bridge::convert_gz_to_ros(lc_model.pose(), part.pose);
-          parts.push_back(part);
+      //     for(std::string color : colors_){
+      //       if (name.find(color) != std::string::npos) {
+      //         part.part.color = part_colors_[color];
+      //       }
+      //     }
+      //     ros_gz_bridge::convert_gz_to_ros(lc_model.pose(), part.pose);
+      //     parts.push_back(part);
 
-          break;
+      //     break;
+      //   }
+      // }
+    }
+
+    // if (camera_type_ == "basic") {
+    //   basic_image_msg_->sensor_pose = sensor_pose;
+
+    //   basic_image_msg_->part_poses.clear();
+    //   basic_image_msg_->tray_poses.clear();
+
+    //   for (ariac_msgs::msg::PartPose &part : parts) {
+    //     basic_image_msg_->part_poses.push_back(part.pose);
+    //   }
+
+    //   for (ariac_msgs::msg::KitTrayPose &tray : trays) {
+    //     basic_image_msg_->tray_poses.push_back(tray.pose);
+    //   }
+
+    //   basic_image_msg_->header.frame_id = frame_name_;
+    //   basic_image_msg_->header.stamp.sec = _gz_msg.header().stamp().sec();
+    //   basic_image_msg_->header.stamp.nanosec = _gz_msg.header().stamp().nsec();
+
+    //   basic_image_pub_->publish(*basic_image_msg_);
+    // }
+    if (camera_type_ == "advanced") {
+      RCLCPP_INFO(ros_node_->get_logger(), "INSIDE IF");
+      for (int i =0; i < kit_trays.size(); i++){
+        for (aprs_interfaces::msg::SlotInfo gear : gears){
+          RCLCPP_INFO_STREAM(ros_node_->get_logger(), "Names: " << gear.name << "\t" << kit_trays[i].name);
+          if (gear.name.find(kit_trays[i].name) != std::string::npos){
+            RCLCPP_INFO_STREAM(ros_node_->get_logger(), "Match: " << gear.name << "\t" << kit_trays[i].name);
+            kit_trays[i].slots.push_back(gear);
+          }
+          RCLCPP_INFO_STREAM(ros_node_->get_logger(), "Length: " << kit_trays[i].slots.size());
         }
       }
-    }
 
-    if (camera_type_ == "basic") {
-      basic_image_msg_->sensor_pose = sensor_pose;
-
-      basic_image_msg_->part_poses.clear();
-      basic_image_msg_->tray_poses.clear();
-
-      for (ariac_msgs::msg::PartPose &part : parts) {
-        basic_image_msg_->part_poses.push_back(part.pose);
+      for (int i = 0; i < gear_trays.size(); i++){
+        for (aprs_interfaces::msg::SlotInfo gear : gears){
+          if (gear.name.find(gear_trays[i].name) != std::string::npos){
+            gear_trays[i].slots.push_back(gear);
+          }
+        }
       }
 
-      for (ariac_msgs::msg::KitTrayPose &tray : trays) {
-        basic_image_msg_->tray_poses.push_back(tray.pose);
-      }
+      RCLCPP_INFO(ros_node_->get_logger(), "After for loops");
 
-      basic_image_msg_->header.frame_id = frame_name_;
-      basic_image_msg_->header.stamp.sec = _gz_msg.header().stamp().sec();
-      basic_image_msg_->header.stamp.nanosec = _gz_msg.header().stamp().nsec();
-
-      basic_image_pub_->publish(*basic_image_msg_);
-    }
-    else if (camera_type_ == "advanced") {
       advanced_image_msg_->sensor_pose = sensor_pose;
 
-      advanced_image_msg_->part_poses = parts;
-      advanced_image_msg_->tray_poses = trays;
+      advanced_image_msg_->trays.kit_trays = kit_trays;
+      advanced_image_msg_->trays.part_trays = gear_trays;
 
       advanced_image_msg_->header.frame_id = frame_name_;
       advanced_image_msg_->header.stamp.sec = _gz_msg.header().stamp().sec();
